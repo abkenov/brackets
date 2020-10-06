5a0e4a9da37491e82f804595f84e7997e52d109b
Ablan Abkenov <abkenov.ablan@gmail.com>
2020-10-06 23:13:16 +0600

    feat: implement brackets problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+	let stack = []
+	let configs = {}
+	let count = 0
+
+	for (let config of bracketsConfig) {
+		configs[config[0]] = config[1]
+	}
+
+	for (let bracket of str) {
+		if (bracket in configs) {
+			if (bracket == configs[bracket] && stack[stack.length-1] == configs[bracket]) {
+				stack.pop()
+				continue
+			}
+			stack.push(bracket)
+		} else {
+			if (stack.length == 0) {
+				return false
+			} else {
+				let top = stack[stack.length-1]
+
+				if (bracket == configs[top]) {
+					stack.pop()
+				}
+			}
+		}
+	}
+	console.log(stack)
+	if (stack.length == 0) {
+		return true
+	} else {
+		return false
+	}
 }
